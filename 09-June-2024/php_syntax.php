b65b1de9d8ed0f19108f0a597d3b1ef32333dcfc
mh.ju1803@gmail.com <mh.ju1803@gmai.com>
2024-06-09 13:15:24 +0600

    first commit

---
+++ 09-June-2024/php_syntax.php
@@ -0,0 +1,19 @@
+<?php
+// full syntax
+
+echo "Hello World <br>";
+
+?>
+
+
+<?
+// open short tag
+
+echo "Hello World whith short tag <br>";
+
+?>
+
+<?="Hello World with short circuit";
+// shot Circuit
+
+?>
